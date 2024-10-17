b58b506b5e78fe13e8785e5e16adf756e2d99ab5
Kalysbek <rrkalysbe@gmail.com>
2024-10-17 19:04:07 +0600

    edited testing

--- src/views/Pages/Reference/District/DistrictList.js
+++ src/views/Pages/Reference/District/DistrictList.js
@@ -1,28 +1,36 @@
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import SweetAlert from "react-bootstrap-sweetalert";
 import { makeStyles } from "@material-ui/core/styles";
-import { Box, TextField } from "@material-ui/core";
-import Button from "components/CustomButtons/Button.js";
-import CustomTable from "components/Table/CustomTable";
-import CustomInput from "components/CustomInput/CustomInput.js";
+import { Box, TextField, Modal, Button, Typography, Paper } from "@material-ui/core";
 import { fetchDistrictList, fetchUpdateDistrict, fetchCreateDistrict } from "redux/actions/reference";
+import CustomTable from "components/Table/CustomTable";
 import styles from "assets/jss/material-dashboard-pro-react/views/sweetAlertStyle.js";
 
-const useStyles = makeStyles(styles);
+const useStyles = makeStyles((theme) => ({
+  ...styles,
+  modalContainer: {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(4),
+    outline: "none",
+    borderRadius: 8,
+  },
+}));
 
 export default function RegionManager() {
   const classes = useStyles();
   const dispatch = useDispatch();
+  const DistrictList = useSelector((state) => state.reference?.districtList);
 
-  
   const [modalOpen, setModalOpen] = useState(false);
-  const [alert, setAlert] = useState(null);
   const [editMode, setEditMode] = useState(false);
   const [editDistrictId, setEditDistrictId] = useState(null);
-  const [regionData, setRegionData] = useState({ name: ""});
-
-  const DistrictList = useSelector((state) => state.reference?.districtList);
+  const [regionData, setRegionData] = useState({ name: "" });
 
   useEffect(() => {
     dispatch(fetchDistrictList());
@@ -36,7 +44,7 @@ export default function RegionManager() {
       disableSortBy: true,
       Cell: ({ row }) => (
         <Box display="flex">
-          <Button variant="outlined" color="info" onClick={() => showInputAlert(row.original)}>
+          <Button variant="outlined" color="info" onClick={() => handleEditClick(row.original)}>
             Изменить
           </Button>
         </Box>
@@ -44,80 +52,37 @@ export default function RegionManager() {
     },
   ];
 
-  useEffect(() => {
-    if(editMode) {
-      setAlert(
-        <SweetAlert
-          showCancel
-          style={{ display: "block", marginTop: "-100px" }}
-          title={editMode ? "Изменить регион" : "Добавить регион"}
-          onConfirm={handleInputConfirm}
-          onCancel={hideAlert}
-          confirmBtnCssClass={`${classes.button} ${classes.info}`}
-          cancelBtnCssClass={`${classes.button} ${classes.danger}`}
-        >
-          <Box>
-           
-               <CustomInput
-                                labelText='Название региона'
-                                formControlProps={{
-                                    fullWidth: true,
-                                }}
-                                inputProps={{
-                                    onChange: event => {
-                                      handleInputChange(event)
-                                    },
-                                    type: 'text',
-                                    name: 'name',
-                                    value: regionData['name'],
-                                }}
+  const handleEditClick = (region) => {
+    console.log(region.id)
+    setEditMode(true);
+    setEditDistrictId(region.id);
+    console.log(editDistrictId,'id')
+    setRegionData({ name: region.name });
+    setModalOpen(true);
+  };
 
-                            />
-          </Box>
-        </SweetAlert>
-      );
-    }
-  }, [editMode]);
-
-  const showInputAlert = (region = {}) => {
-    const isEditMode = !!region.id;
-    setEditMode(isEditMode);
-    setEditDistrictId(isEditMode ? region.id : null);
-
-    if(isEditMode) {
-     
-      setRegionData(region)
-      console.log(regionData)
-    }
-
-
+  const handleAddClick = () => {
+    setEditMode(false);
+    setRegionData({ name: "" });
+    setModalOpen(true);
   };
 
   const handleInputChange = (e) => {
-    
-
-    const { name, value, type } = e.target;
-    console.log(name,value)
-  
-    const newValue = type === 'number' ? Number(value) : value;
-    console.log(newValue)
-    setRegionData((prevData) => ({
-        ...prevData,
-        [name]: newValue,
-    }));
+    const { value } = e.target;
+    setRegionData({ name: value });
   };
 
-  const handleInputConfirm = () => {
+  const handleConfirm = () => {
     if (editMode && editDistrictId) {
       dispatch(fetchUpdateDistrict(editDistrictId, regionData));
     } else {
       dispatch(fetchCreateDistrict(regionData));
     }
-    hideAlert();
+    handleCloseModal();
   };
 
-  const hideAlert = () => {
-    setAlert(null);
+  const handleCloseModal = () => {
+    setModalOpen(false);
     setEditMode(false);
     setEditDistrictId(null);
     setRegionData({ name: "" });
@@ -125,13 +90,35 @@ export default function RegionManager() {
 
   return (
     <div>
-      {alert}
       <Box display="flex" justifyContent="flex-end" mb={2}>
-        <Button color="success" onClick={() => showInputAlert()}>
+        <Button color="success" onClick={handleAddClick}>
           Добавить регион
         </Button>
       </Box>
       <CustomTable tableName="Регионы" tableHead={tableHeaders} tableData={DistrictList} searchKey="name" />
+      
+      <Modal open={modalOpen} onClose={handleCloseModal}>
+        <Paper className={classes.modalContainer}>
+          <Typography variant="h6">
+            {editMode ? "Изменить регион" : "Добавить регион"}
+          </Typography>
+          <TextField
+            label="Название региона"
+            fullWidth
+            value={regionData.name}
+            onChange={handleInputChange}
+            margin="normal"
+          />
+          <Box mt={2} display="flex" justifyContent="flex-end">
+            <Button onClick={handleConfirm} color="primary">
+              {editMode ? "Сохранить" : "Добавить"}
+            </Button>
+            <Button onClick={handleCloseModal} color="secondary">
+              Отмена
+            </Button>
+          </Box>
+        </Paper>
+      </Modal>
     </div>
   );
 }
